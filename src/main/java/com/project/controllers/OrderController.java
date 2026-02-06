d5aed774fc46d48cbc5713ff8145e03da42c1711
ShubhamSharma <Shubhamsharma1122333@gmail.com>
2026-02-06 12:34:53 +0530

    driver controller updated

--- src/main/java/com/project/controllers/OrderController.java
+++ src/main/java/com/project/controllers/OrderController.java
@@ -1,5 +1,13 @@
 package com.project.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+
 public class OrderController {
+
 
 }
